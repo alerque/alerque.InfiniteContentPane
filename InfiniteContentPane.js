c5fd1f86fa270c5dce33ba82bfd8f3cc75b7b8df
caleb <caleb@alerque.com>
2011-02-18 01:46:16 +0200

    Starting the wiring.

--- InfiniteContentPane.js
+++ InfiniteContentPane.js
@@ -6,6 +6,8 @@ dojo.provide("dojox.layout.InfiniteContentPane");
 dojo.declare("dojox.layout.InfiniteContentPane",
 		[dijit.layout.ContentPane],
 {
+	fetcher: false,
+
 	_onScroll: function() {
 		console.log(this, arguments);
 	}
