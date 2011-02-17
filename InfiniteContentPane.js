2f4f61083b618b170ea5492ec06c07ab5bed08c6
steve <icsteveoh@gmail.com>
2011-02-17 15:37:10 -0800

    added some plumbing for getting the height and the buffer to be used to find out if you need to fetch new content.

--- InfiniteContentPane.js
+++ InfiniteContentPane.js
@@ -3,9 +3,19 @@ dojo.experimental("dojox.layout.InfiniteContentPane");
 
 dojo.provide("dojox.layout.InfiniteContentPane");
 
-dojo.declare("dojox.layout.InfiniteContentPane",
-		[dijit.layout.ContentPane],
-{
+dojo.declare("dojox.layout.InfiniteContentPane", [dijit.layout.ContentPane], {
+    //buffer to initiate request while scrolling to make seemless.  
+    bufferHeightPx: 0,
+    containerHeight: 0,
+
+    postCreate: function () {
+        this.inherited(arguments);
+
+        //if we use this we'll have to watch resize and reset it if the content pane is a %
+        this.containerHeight = dojo.marginBox(this.domNode).h;
+        //make buffer size 10% of total height?
+        this.bufferHeightPx = this.containerHeight * .10;
+    },
 	_onScroll: function() {
 		console.log(this, arguments);
 	}
