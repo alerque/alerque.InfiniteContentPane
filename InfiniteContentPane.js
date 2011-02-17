142e7823b1bf4c42be4340fb472e13d39d34fc90
caleb <caleb@alerque.com>
2011-02-18 01:24:11 +0200

    Added dojo.provide() and more useful test content.

--- InfiniteContentPane.js
+++ InfiniteContentPane.js
@@ -1,7 +1,12 @@
 dojo.require("dijit.layout.ContentPane");
 dojo.experimental("dojox.layout.InfiniteContentPane"); 
 
+dojo.provide("dojox.layout.InfiniteContentPane");
+
 dojo.declare("dojox.layout.InfiniteContentPane",
 		[dijit.layout.ContentPane],
 {
+	_onScroll: function() {
+		console.log(this, arguments);
+	}
 });
