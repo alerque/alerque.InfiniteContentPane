0d7204e30b7869998a91dcf6747824c97b6eb58c
steveoh <sgourley@utah.gov>
2011-02-17 20:16:53 -0700

    modified fetch to use xhr to test data

--- InfiniteContentPane.js
+++ InfiniteContentPane.js
@@ -1,68 +1,73 @@
 dojo.require("dijit.layout.ContentPane");
-dojo.experimental("dojox.layout.InfiniteContentPane"); 
+dojo.experimental("dojox.layout.InfiniteContentPane");
 
 dojo.provide("dojox.layout.InfiniteContentPane");
 
 dojo.declare("dojox.layout.InfiniteContentPane",
 		[dijit.layout.ContentPane],
 {
-	fetcher: false, // dojo.Deferred given us for returning the next content
-	triggerZoneSize: 100, // hot zone that triggers a fetch needs to be fixed height, percentages would make it funky as more content gets loaded it would get too big
-	fetchCount: 0, // Iterator showing how many times we've expanded. Might be useful to return to our fetcher
+    fetcher: false, // dojo.Deferred given us for returning the next content
+    triggerZoneSize: 100, // hot zone that triggers a fetch needs to be fixed height, percentages would make it funky as more content gets loaded it would get too big
+    fetchCount: 0, // Iterator showing how many times we've expanded. Might be useful to return to our fetcher
 
     _paneHeight: 0, // this is private because it can't be set externally, it's just the size we read ourselves to be
-	_scrollHeight: 0,
+    _scrollHeight: 0,
 
     postCreate: function () {
-		this.connect(this.domNode, "onscroll", "_onScroll");
+        this.connect(this.domNode, "onscroll", "_onScroll");
 
-		this._calc();
+        this._calc();
 
-		// Connect an onresize function to our parent pane to _calc and _onScroll
+        // Connect an onresize function to our parent pane to _calc and _onScroll
 
         return this.inherited(arguments);
     },
 
-	_calc: function() {
+    _calc: function () {
         this._paneHeight = dojo._getMarginSize(this.domNode).h;
-		this._scrollHeight = this.domNode['scrollHeight'];
-	},
+        this._scrollHeight = this.domNode['scrollHeight'];
+    },
 
-	_onScroll: function(/* Event */e) {
-		var bottomPos = this.domNode['scrollTop'] + this._paneHeight;
+    _onScroll: function (/* Event */e) {
+        var bottomPos = this.domNode['scrollTop'] + this._paneHeight;
 
-		if (bottomPos > (this._scrollHeight - this.triggerZoneSize)) {
-			this._fetch();
-		}
+        if (bottomPos > (this._scrollHeight - this.triggerZoneSize)) {
+            this._fetch();
+        }
 
-		// Find our current position
+        // Find our current position
 
-		// test if trigger zone visiable
-		
-		// set timeout so we don't fire to often?
+        // test if trigger zone visiable
 
-		// notify our fetcher that we need data. pass back count? or position data?
+        // set timeout so we don't fire to often?
 
-	 	// disconnect scroll notifier until we get previous data?
-	},
+        // notify our fetcher that we need data. pass back count? or position data?
 
-	_fetch: function() {
-		// Do something with the deferred fetcher we were given
-		// Connect it's clalback to our data handler
+        // disconnect scroll notifier until we get previous data?
+    },
 
-		// For debug, just give us soething
-		var demo_data = dojo.create("p", {innerHTML: 'demo more data: ' + this.fetchCount})
-		this._fetcherCallback(demo_data);
-		this.fetchCount += 1;
-	},
+    _fetch: function () {
+        // Do something with the deferred fetcher we were given
+        // Connect it's clalback to our data handler
 
-	_fetcherCallback: function(data) {
-		// handle data comming in from the fetcher
-		dojo.place(data, this.domNode, 'last');
-		
-		// Update our knowledge about ourselves now that we stuffed new data
-		this._calc();
+        // For debug, just give us something. 
 
-		// reactivate scroll watcher if suspended above
-	}
+        //would you realistically use the fether deferred here?
+        var d = dojo.xhrGet({
+            url: "extra_content.html",
+            content: { fragment: ++this.fetchCount }
+        });
+
+        dojo.when(d, dojo.hitch(this, this._fetcherCallback));
+    },
+
+    _fetcherCallback: function (data) {
+        // handle data comming in from the fetcher
+        dojo.place(data, this.domNode, 'last');
+
+        // Update our knowledge about ourselves now that we stuffed new data
+        this._calc();
+
+        // reactivate scroll watcher if suspended above
+    }
 });
