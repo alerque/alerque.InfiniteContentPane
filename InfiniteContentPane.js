2959e1d2357ab72229646a681f2df5920eb8ba2b
caleb <caleb@alerque.com>
2011-02-18 11:05:01 +0200

    Comment cleanup

--- InfiniteContentPane.js
+++ InfiniteContentPane.js
@@ -6,7 +6,7 @@ dojo.provide("dojox.layout.InfiniteContentPane");
 dojo.declare("dojox.layout.InfiniteContentPane",
 		[dijit.layout.ContentPane],
 {
-	fetcher: false, // dojo.Deferred given us for returning the next content
+	fetcher: null, // dojo.Deferred given us for returning the next content
 	triggerZoneSize: 100, // hot zone that triggers a fetch needs to be fixed height, percentages would make it funky as more content gets loaded it would get too big
 	fetchCount: 0, // Iterator showing how many times we've expanded. Might be useful to return to our fetcher
 
@@ -19,12 +19,13 @@ dojo.declare("dojox.layout.InfiniteContentPane",
 
 		this._calc();
 
-		// Connect an onresize function to our parent pane to _calc and _onScroll
+		// TODO: Connect an onresize function to our parent pane to _calc and _onScroll
 
 		return this.inherited(arguments);
 	},
 
 	_calc: function () {
+		// TODO: do some match to make sure trigger zone is a reasonable size of pane?
 		this._paneHeight = dojo._getMarginSize(this.domNode).h;
 		this._scrollHeight = this.domNode['scrollHeight'];
 	},
@@ -39,12 +40,14 @@ dojo.declare("dojox.layout.InfiniteContentPane",
 			this._fetch();
 		}
 
-		// set timeout so we don't fire to often?
-		// disconnect scroll notifier until we get previous data?
+		// TODO: set timeout so we don't fire to often?
+		// TODO: disconnect scroll notifier until we get previous data?
 	},
 
 	_fetch: function () {
 		this.fetchCount += 1;
+
+		// TODO: show loading content?
 
 		// Wire up the fetcher we were given to our internal data handler.
 		// The retun value of the fetchers fetch function should indicate
@@ -60,16 +63,20 @@ dojo.declare("dojox.layout.InfiniteContentPane",
 		if (!data.length) {
 			return this._disable();
 		}
-		// handle data comming in from the fetcher
+
+		// TODO: trigger parser if parseOnLoad is true
+
+		// Append stuff comming in from the fetcher to the pane
 		dojo.place(data, this.domNode, 'last');
 
 		// Update our knowledge about ourselves now that we stuffed new data
 		this._calc();
 
-		// reactivate scroll watcher if suspended above
+		// TODO: reactivate scroll watcher if suspended above
 	},
 
 	_disable: function() {
+		// If we stop getting data, unwire the scroll event to save resources
 		dojo.disconnect(this._connect[0]);
 	}
 
