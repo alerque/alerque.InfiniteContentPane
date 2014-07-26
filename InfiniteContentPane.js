cd5e3fc47bc2f078752324976c2b1b9df9941e06
Caleb Maclennan <caleb@alerque.com>
2014-07-26 15:16:42 +0300

    normalize whitespace style

--- InfiniteContentPane.js
+++ InfiniteContentPane.js
@@ -1,119 +1,119 @@
 // module:
-//		alerque/InfiniteContentPane
+//    alerque/InfiniteContentPane
 // summary:
-//		A layout widget for retrieving extra content on scroll so it never runs
-//		out of something to show.
+//    A layout widget for retrieving extra content on scroll so it never runs
+//    out of something to show.
 
 define([
-	"dojo/_base/declare",
-	"dojo/_base/lang",
+  "dojo/_base/declare",
+  "dojo/_base/lang",
   "dojo/Deferred",
   "dojo/dom-construct",
   "dojo/dom-geometry",
   "dojo/html",
   "dojo/on",
   "dojo/parser",
-  "dojox/layout/ContentPane",
+  "dojox/layout/ContentPane"
 ], function(declare, lang, Deferred, domConstruct, domGeometry, html, on,
             parser, ContentPane) {
 return declare("alerque.InfiniteContentPane", [ContentPane], {
   // a user supplied function of some kind that returns data
-	fetcher: null,
+  fetcher: null,
   // hot zone that triggers a fetch, fixed height in pixels
-	triggerHeight: 100,
+  triggerHeight: 100,
   // how many pending fetcher threads to allow
-	maxFetchers: 1,
-	loadingMsg: '<p>Loading...</p>',
+  maxFetchers: 1,
+  loadingMsg: '<p>Loading...</p>',
   enableUp: false,
 
-	_paneHeight: 0,
-	_scrollHeight: 0,
+  _paneHeight: 0,
+  _scrollHeight: 0,
   // Iterator showing how many times we've expanded. Might be useful to return
   // to our fetcher
-	_fetcherCount: 0,
-	_fetchersCount: 0,
+  _fetcherCount: 0,
+  _fetchersCount: 0,
   // a handle for our on scroll event so we can shut it off the workings if we
   // run out of data
-	_connect: null,
+  _connect: null,
   _heightMark: 0,
 
-	postCreate: function () {
+  postCreate: function () {
     // Wire up scroll events to checking if we need more data
-		this._connect = on(this.domNode, "scroll", lang.hitch(this, '_onScroll'));
+    this._connect = on(this.domNode, "scroll", lang.hitch(this, '_onScroll'));
     // Run a check on our data situation on instantiation
-		this._calc();
+    this._calc();
     this._onScroll();
-		return this.inherited(arguments);
-	},
+    return this.inherited(arguments);
+  },
 
-	resize: function() {
-		// if we got resized, recalculate our size and then simulate a scroll event
+  resize: function() {
+    // if we got resized, recalculate our size and then simulate a scroll event
     // just to make sure we have the data we're supposted to
-		this._calc();
-		this._onScroll();
-		return this.inherited(arguments);
-	},
+    this._calc();
+    this._onScroll();
+    return this.inherited(arguments);
+  },
 
   _calc: function () {
-		// TODO: do some math to make sure trigger zone is a sane size of pane
-		this._paneHeight = domGeometry.position(this.domNode).h;
-		this._scrollHeight = this.domNode['scrollHeight'];
-	},
+    // TODO: do some math to make sure trigger zone is a sane size of pane
+    this._paneHeight = domGeometry.position(this.domNode).h;
+    this._scrollHeight = this.domNode['scrollHeight'];
+  },
 
-	_onScroll: function (event) {
-		// Find our current position
-		var bottomPos = this.domNode['scrollTop'] + this._paneHeight;
+  _onScroll: function (event) {
+    // Find our current position
+    var bottomPos = this.domNode['scrollTop'] + this._paneHeight;
 
-		// Do the math to see if the trigger zone area has scrolled into view
-		if (bottomPos > (this._scrollHeight - this.triggerHeight)) {
-			// As long as we aren't waiting on too much already, go fetch data
-			if (this._fetchersCount < this.maxFetchers) {
-				this._fetch(false);
-			}
-		}
+    // Do the math to see if the trigger zone area has scrolled into view
+    if (bottomPos > (this._scrollHeight - this.triggerHeight)) {
+      // As long as we aren't waiting on too much already, go fetch data
+      if (this._fetchersCount < this.maxFetchers) {
+        this._fetch(false);
+      }
+    }
 
     if (this.enableUp) {
       if (this.domNode['scrollTop'] < this.triggerHeight) {
         this._fetch(true);
       }
     }
-	},
+  },
 
-	_fetch: function (isUp) {
-		// TODO: test that fetcher is a function? object? In any case don't bother
+  _fetch: function (isUp) {
+    // TODO: test that fetcher is a function? object? In any case don't bother
     // if we don't have one
-		if (!this.fetcher) {
-			return this._disable();
-		}
-		this._fetcherCount += 1;
+    if (!this.fetcher) {
+      return this._disable();
+    }
+    this._fetcherCount += 1;
 
-		// Start a placeholder for content that we'll be fetching. Doing this early
+    // Start a placeholder for content that we'll be fetching. Doing this early
     // lets us set a loading message and keeps content in order even if async
     // fetchers come back out of order.
-		var wrapper = domConstruct.create('div', {
+    var wrapper = domConstruct.create('div', {
       'class': 'alerque-infinite-content'
     });
-		domConstruct.place(wrapper, this.domNode, isUp ? 'first' : 'last');
+    domConstruct.place(wrapper, this.domNode, isUp ? 'first' : 'last');
     this._setFetchedContent(wrapper, this.loadingMsg, isUp);
 
 
     // Instantiate a new fetcher
     var fetcher = this._runFetcher(this.fetcher, wrapper, this._fetcherCount, isUp);
-		this._fetchersCount++;
+    this._fetchersCount++;
 
     fetcher.then(lang.hitch(this, function(result) {
       // Scan for dojo declarative markup in new content
-			if (this.parseOnLoad) {
-				parser.parse(wrapper);
-			}
-			// Update our knowledge about ourselves now that we stuffed new data
-			this._calc();
-			this._fetchersCount--;
+      if (this.parseOnLoad) {
+        parser.parse(wrapper);
+      }
+      // Update our knowledge about ourselves now that we stuffed new data
+      this._calc();
+      this._fetchersCount--;
     }), lang.hitch(this, function(err){
       // If the fetcher is rejecting our request, unwire it from out widget
       return this._disable();
     }));
-	},
+  },
 
   // Wrap the user supplied content generator funtion in a deferred object to
   // make it an async source no matter where the data is coming from
@@ -135,7 +135,7 @@ return declare("alerque.InfiniteContentPane", [ContentPane], {
 
     return deferred.promise;
   },
-  
+
   _setFetchedContent: function(node, content, isUp) {
     var marker = this.domNode['scrollHeight'];
     html.set(node, content);
@@ -144,10 +144,10 @@ return declare("alerque.InfiniteContentPane", [ContentPane], {
     }
   },
 
-	_disable: function() {
-		// If we stop getting data, unwire the scroll event to save resources
+  _disable: function() {
+    // If we stop getting data, unwire the scroll event to save resources
     this._connect.remove();
-	}
+  }
 
 });
 
